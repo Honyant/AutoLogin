641272afaff6b64d01520f4a02d535ee9e44b6c9
Neil Song <song.neil.song@gmail.com>
2023-02-21 10:20:00 -0600

    migrate to manifest v3, bump version

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.action.onClicked.addListener((tab) => {
   var u = prompt("What is your email?");
   if (u != null) {
     chrome.storage.local.set({ u: u }, function () {
